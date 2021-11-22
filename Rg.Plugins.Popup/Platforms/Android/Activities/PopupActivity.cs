92e06285cf7b697cf44288024693ffe4ccc794f4
John Wilson <john.wilson@ato.gov.au>
2021-11-22 14:09:46 +1000

    override PopupActivity 'enter' default animation with our new one, seems to work OK. Also override the 'exit' animation, which doesn't appear to do anything, but now the black line 'mask' is gone.

--- Rg.Plugins.Popup/Platforms/Android/Activities/PopupActivity.cs
+++ Rg.Plugins.Popup/Platforms/Android/Activities/PopupActivity.cs
@@ -7,13 +7,24 @@ using Android.Widget;
 namespace Rg.Plugins.Popup.Platforms.Android.Activities
 {
     [Activity(Label = "", Theme = "@android:style/Theme.Translucent.NoTitleBar", NoHistory = true)]
-    public class PopupActivity : Activity
+    public class PopupActivity : Activity 
     {
         private View? _popupView;
+        private int _enterAnimationResourceId;
+        private int _exitAnimationResourceId;
+        private int _noAnimationResourceId;
+
+        //keep trying to find a way to override the animation/transition for this activity to replace the apparent 'wipe down' with something else. 
         
         protected override void OnCreate(Bundle? savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
+
+            _enterAnimationResourceId = Popup.Context.Resources.GetIdentifier("enter_activity", "anim", "au.gov.ato.ATOTax");
+            _exitAnimationResourceId = Popup.Context.Resources.GetIdentifier("exit_activity", "anim", "au.gov.ato.ATOTax");
+            _noAnimationResourceId = Popup.Context.Resources.GetIdentifier("None", "anim", "au.gov.ato.ATOTax");
+            
+            OverridePendingTransition(_enterAnimationResourceId, _noAnimationResourceId);
 
             var popupIdVal = Intent?.GetStringExtra("popupId");
 
@@ -30,14 +41,27 @@ namespace Rg.Plugins.Popup.Platforms.Android.Activities
             Popup.Context = this;
 
             // NOTE: neither of these options are ideal
-            var flags = WindowManagerFlags.ForceNotFullscreen; // Results in an ugly animated status bar mask
-            // var flags = WindowManagerFlags.Fullscreen;      // Result in status bar being toggled
+            // var flags = WindowManagerFlags.Fullscreen;      // Result in status bar being toggled/pushed off screen
+            var flags = WindowManagerFlags.ForceNotFullscreen; // works perfectly but Results in a quick but ugly downward moving mask on the way out
+
             Window?.SetFlags(flags, flags);
 
             var decorView = (FrameLayout?)Window?.DecorView;
             decorView?.AddView(_popupView);
         }
+        
+        public override void Finish()
+        {
+            base.Finish();
+            OverridePendingTransition(_exitAnimationResourceId, _exitAnimationResourceId);
+        }
 
+        public override void OnBackPressed()
+        {
+            base.OnBackPressed();
+            Finish();
+        }
+        
         protected override void OnDestroy()
         {
             base.OnDestroy();
@@ -48,12 +72,6 @@ namespace Rg.Plugins.Popup.Platforms.Android.Activities
                 decorView?.RemoveView(_popupView);
             }
         }
-
-        public override void OnBackPressed()
-        {
-            base.OnBackPressed();
-
-            Finish();
-        }
+        
     }
 }
